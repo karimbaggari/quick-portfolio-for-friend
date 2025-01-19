efd400c8984f970e134b2149a1b9d37489c8f80a
karim <kariim.baggarii@gmail.com>
2025-01-19 23:43:11 +0100

    gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/footer";
+import Gallery from "@/components/gallery";
+import Navbar from "@/components/navbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <Gallery />
+      <Footer />
+    </>
+  );
+};
+
+export default page;
