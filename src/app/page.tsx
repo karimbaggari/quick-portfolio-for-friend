58cbdb96b5488ace78f6f67d61e036ba40dabf76
karim <kariim.baggarii@gmail.com>
2025-01-19 23:14:49 +0100

    add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
+import LogoCloud from "@/components/logoCloud";
 import Navbar from "@/components/navbar";
+import Reviews from "@/components/reviews";
+import Section from "@/components/section";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Section />
+      <LogoCloud />
+      <Reviews />
+      <Footer />
     </>
   );
 }
